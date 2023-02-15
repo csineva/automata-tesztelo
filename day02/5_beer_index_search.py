68d8d893893de8b6f9ecb7e863d7c91e725ab459
benedek_istvan <csineva@gmail.com>
2023-02-15 07:44:23 +0100

    4_dice_game.py added

---
+++ day02/5_beer_index_search.py
@@ -0,0 +1,34 @@
+"""---------------------------------------------------------------------------------------------------------------------
+SZORGALMI
+A megadott stringben keressük meg az összes 'sör' előfordulást és tároljuk el az indexüket egy listában
+"""
+import time
+
+sentence = 'Egy sör nem sör. 2 sör fél sör. 4 sör 1 sör. 1 sör meg nem sör...'
+sorindexek = []
+vege = False
+pointer = 0
+
+print("Sörkeresés indul az alábbi mintában:")
+for i in sentence:
+    print(i, end='')
+    time.sleep(.05)
+
+print()
+print('-' * len(sentence))
+time.sleep(2)
+
+while not vege:
+    pointer = sentence.find('sör', pointer)
+    if pointer > -1:
+        sorindexek.append(pointer)
+        print('Hoppá! Nicsak, itt is egy sör:', pointer)
+        pointer += 1
+
+        time.sleep(.5)
+    else:
+        vege = True
+print()
+print('*' * len(sentence))
+print('Sörökkévalóság!', sorindexek)
+print('*' * len(sentence))
