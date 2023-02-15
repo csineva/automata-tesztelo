635233891bd02dd1a4ace233b4dddc5f35b6f638
benedek_istvan <csineva@gmail.com>
2023-02-15 12:40:18 +0100

    10 mod

--- day3/p10_prints.py
+++ day3/p10_prints.py
@@ -5,3 +5,22 @@ I. Írasd ki egyesével a képernyőre, egymás alá a 10-nél kisebb számokat!
 II. Gyűjtsd egy listába a 20-nál nagyobb számokat! Jelenítsd meg a listaelemeket és hogy hány elemű ez a lista!
 III. Növekvő sorrendben írasd ki a számokat egyesével, egy sorba!
 '''
+mylist = [15, 56, 1, 89, 2, 78, 9, 40, 10, 32]
+twenty = []
+for i in mylist:
+    if i < 10:
+        print(i)
+
+
+for i in mylist:
+    if i > 20:
+        twenty.append(i)
+print(twenty, 'length: ', len(twenty))
+
+
+for i in sorted(mylist):
+    print(i)
+
+
+
+
