9b5a1c6c2b75a84837f5886356297fa2abd37535
benedek_istvan <csineva@gmail.com>
2023-02-16 07:13:45 +0100

    p11_palindrome modified

--- day3/szorgalmi/p15_nested.py
+++ day3/szorgalmi/p15_nested.py
@@ -8,5 +8,23 @@ Rajzold ki az alábbi ábrát egy szkript segítségével!
 +++++
 '''
 
-for i in range(1,6):
+for i in range(1, 6):
     print('+' * i)
+
+# some variations
+
+# print()
+# for i in range(1, 10, 2):
+#     s = '+' * i
+#     print(f'{s : ^10}')
+#
+# print()
+# for i in range(1, 10, 2):
+#     s = '+' * i
+#     print(f'{s : <10} {s : ^10} {s : >10}')
+#
+# print()
+# for i in range(1, 6):
+#     s = '+' * i
+#     s2 = '+' * 2 * i
+#     print(f'{s : <5} {s2 : ^10} {s : >5}')
