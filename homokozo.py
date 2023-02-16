0cb9323d169e4d65b462afbba95365495a0ed131
benedek_istvan <csineva@gmail.com>
2023-02-16 10:34:33 +0100

    homokozoo added

---
+++ homokozo.py
@@ -0,0 +1,9 @@
+PI = 3.14159265359
+print("A pí értéke: {}".format(PI))                   # format() metódus
+
+nev = "Béla"
+eletkor = 21
+print("Hello, %s vagyok, %d éves." % (nev, eletkor))  # %-formatting
+
+elet_ertelme = 42
+print(f"Az élet értelme: {elet_ertelme}")             # f-string
